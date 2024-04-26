c84ad1d36e4dfd4476574790659764620e48ba95
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2024-04-25 22:06:49 -0400

    A start

---
+++ python-src/codebook.py
@@ -0,0 +1,250 @@
+from enum import Enum
+
+class MisconceptionCode(Enum):
+    Precedence = "Precedence"
+    ReasonableVariant = "ReasonableVariant"
+    Unlabeled = "Unlabeled"
+    BadProp = "BadProp"
+    BadStateIndex = "BadStateIndex"
+    BadStateQuantification = "BadStateQuantification"
+    ExclusiveU = "ExclusiveU"
+    ImplicitF = "ImplicitF"
+    ImplicitG = "ImplicitG"
+    OtherImplicit = "OtherImplicit"
+    WeakU = "WeakU"
+
+
+class MutationResult:
+    def __init__(self, node, misconception=None):
+        self.node = node
+        self.misconception = misconception
+
+
+def applyMisconception(node, misconception):
+    if misconception == MisconceptionCode.Precedence:
+        return applyTilFirst(node, applyPrecedence)
+    elif misconception == MisconceptionCode.BadStateIndex:
+        return applyTilFirst(node, applyBadStateIndex)
+    elif misconception == MisconceptionCode.BadStateQuantification:
+        return applyTilFirst(node, applyBadStateQuantification)
+    elif misconception == MisconceptionCode.ExclusiveU:
+        return applyTilFirst(node, applyExclusiveU)
+    elif misconception == MisconceptionCode.ImplicitF:
+        return applyTilFirst(node, applyImplicitF)
+    elif misconception == MisconceptionCode.ImplicitG:
+        return applyTilFirst(node, applyImplicitG)
+    elif misconception == MisconceptionCode.WeakU:
+        return applyTilFirst(node, applyWeakU)
+    elif misconception == MisconceptionCode.OtherImplicit:
+        res = applyTilFirst(node, applyImplicitPrefix)
+        if not res.misconception:
+            return applyTilFirst(node, applyUnderconstraint)
+        return res
+    elif misconception in [MisconceptionCode.BadProp, MisconceptionCode.Unlabeled, MisconceptionCode.ReasonableVariant]:
+        return MutationResult(node)
+    else:
+        return MutationResult(node)
+
+
+def getAllApplicableMisconceptions(node):
+    return [
+        applyMisconception(node, misconception)
+        for misconception in MisconceptionCode
+        if applyMisconception(node, misconception).misconception is not None
+    ]
+
+
+def applyTilFirst(node, f):
+    res = f(node)
+    if res.misconception:
+        return res
+
+    if isinstance(node, UnaryOperatorNode):
+        res = applyTilFirst(node.operand, f)
+        node.operand = res.node
+        res.node = node
+        return res
+
+    if isinstance(node, BinaryOperatorNode):
+        res_left = applyTilFirst(node.left, f)
+        res_right = applyTilFirst(node.right, f)
+
+        random_index = random.randint(0, 1)
+        choose_left = res_left.misconception and (not res_right.misconception or random_index == 0)
+        choose_right = res_right.misconception and (not res_left.misconception or random_index == 1)
+
+        if choose_left:
+            node.left = res_left.node
+            res_left.node = node
+            return res_left
+        elif choose_right:
+            node.right = res_right.node
+            res_right.node = node
+            return res_right
+
+    return MutationResult(node)
+
+
+def applyPrecedence(node):
+    if isinstance(node, BinaryOperatorNode):
+        if isinstance(node.right, BinaryOperatorNode):
+            new_top = node.right
+            node.right = new_top.left
+            new_top.left = node
+            return MutationResult(new_top, MisconceptionCode.Precedence)
+
+    return MutationResult(node)
+
+
+def applyExclusiveU(node):
+    if isinstance(node, BinaryOperatorNode) and node.operator == "U":
+        x = node.left
+        rhs = node.right
+
+        if isinstance(rhs, BinaryOperatorNode) and rhs.operator == "and":
+            y = rhs.right
+
+            if isinstance(rhs.left, UnaryOperatorNode) and rhs.left.operator == "!" and LTLNode.equiv(rhs.left.operand, x):
+                return MutationResult(UntilNode(x, y), MisconceptionCode.ExclusiveU)
+
+    return MutationResult(node)
+
+
+def applyImplicitG(node):
+    if isinstance(node, UnaryOperatorNode) and node.operator == "G":
+        return MutationResult(node.operand, MisconceptionCode.ImplicitG)
+
+    return MutationResult(node)
+
+
+def applyImplicitF(node):
+    if isinstance(node, UnaryOperatorNode) and node.operator == "F":
+        return MutationResult(node.operand, MisconceptionCode.ImplicitF)
+
+    return MutationResult(node)
+
+
+def applyImplicitPrefix(node):
+    if isinstance(node, UntilNode):
+        lhs = node.left
+        rhs = node.right
+
+        if isinstance(lhs, NotNode) and LTLNode.equiv(lhs.operand, rhs):
+            return MutationResult(FinallyNode(rhs), MisconceptionCode.OtherImplicit)
+
+        if isinstance(rhs, GloballyNode) and LTLNode.equiv(lhs, rhs.operand):
+            return MutationResult(AndNode(lhs, FinallyNode(rhs)), MisconceptionCode.OtherImplicit)
+
+    if isinstance(node, AndNode):
+        lhs = node.left
+        rhs = node.right
+
+        if isinstance(lhs, FinallyNode) and isinstance(rhs, GloballyNode):
+            rhs_operand = rhs.operand
+            if isinstance(rhs_operand, ImpliesNode):
+                rhs_operand_lhs = rhs_operand.left
+                rhs_operand_rhs = rhs_operand.right
+
+                if LTLNode.equiv(lhs.operand, rhs_operand_lhs) and isinstance(rhs_operand_rhs, NextNode) and isinstance(rhs_operand_rhs.operand, GloballyNode) and LTLNode.equiv(rhs_operand_lhs, rhs_operand_rhs.operand.operand):
+                    return MutationResult(AndNode(lhs.operand, NextNode(rhs)), MisconceptionCode.OtherImplicit)
+
+    if isinstance(node, NextNode):
+        return MutationResult(FinallyNode(node.operand), MisconceptionCode.OtherImplicit)
+
+    return MutationResult(node)
+
+
+def applyUnderconstraint(node):
+    if isinstance(node, BinaryOperatorNode):
+        return MutationResult(node.left, MisconceptionCode.OtherImplicit)
+    elif isinstance(node, UnaryOperatorNode):
+        return MutationResult(node.operand, MisconceptionCode.OtherImplicit)
+    else:
+        return MutationResult(node)
+
+
+def applyWeakU(node):
+    if isinstance(node, UntilNode):
+        rhs = node.right
+        new_node = AndNode(node, FinallyNode(rhs))
+        return MutationResult(new_node, MisconceptionCode.WeakU)
+
+    return MutationResult(node)
+
+
+def applyBadStateQuantification(node):
+    if isinstance(node, GloballyNode):
+        op = node.operand
+        return MutationResult(FinallyNode(op), MisconceptionCode.BadStateQuantification)
+
+    if isinstance(node, FinallyNode):
+        op = node.operand
+        return MutationResult(GloballyNode(op), MisconceptionCode.BadStateQuantification)
+
+    if isinstance(node, UntilNode):
+        lhs = node.left
+        rhs = node.right
+
+        random_num = random.randint(1, 5)
+        new_node = None
+
+        if random_num == 1:
+            new_node = UntilNode(FinallyNode(lhs), rhs)
+        elif random_num == 2:
+            new_node = UntilNode(GloballyNode(lhs), rhs)
+        elif random_num == 3:
+            new_node = UntilNode(lhs, FinallyNode(rhs))
+        elif random_num == 4:
+            new_node = UntilNode(lhs, GloballyNode(rhs))
+        else:
+            new_node = UntilNode(rhs, lhs)
+
+        return MutationResult(new_node, MisconceptionCode.BadStateQuantification)
+
+    return MutationResult(node)
+
+
+def applyBadStateIndex(node):
+    if isinstance(node, UntilNode):
+        lhs = node.left
+        rhs = node.right
+
+        if isinstance(rhs, AndNode) and isinstance(rhs.right, FinallyNode):
+            new_node = AndNode(UntilNode(lhs, rhs.left), rhs.right)
+            return MutationResult(new_node, MisconceptionCode.BadStateIndex)
+
+        if isinstance(rhs, AndNode) and isinstance(rhs.right, GloballyNode):
+            new_node = AndNode(UntilNode(lhs, rhs.left), rhs.right)
+            return MutationResult(new_node, MisconceptionCode.BadStateIndex)
+
+        if isinstance(rhs, OrNode) and isinstance(rhs.right, FinallyNode):
+            new_node = OrNode(UntilNode(lhs, rhs.left), rhs.right)
+            return MutationResult(new_node, MisconceptionCode.BadStateIndex)
+
+        if isinstance(rhs, OrNode) and isinstance(rhs.right, GloballyNode):
+            new_node = OrNode(UntilNode(lhs, rhs.left), rhs.right)
+            return MutationResult(new_node, MisconceptionCode.BadStateIndex)
+
+        if isinstance(rhs, ImpliesNode) and isinstance(rhs.right, FinallyNode):
+            new_node = ImpliesNode(UntilNode(lhs, rhs.left), rhs.right)
+            return MutationResult(new_node, MisconceptionCode.BadStateIndex)
+
+        if isinstance(rhs, ImpliesNode) and isinstance(rhs.right, GloballyNode):
+            new_node = ImpliesNode(UntilNode(lhs, rhs.left), rhs.right)
+            return MutationResult(new_node, MisconceptionCode.BadStateIndex)
+
+    if isinstance(node, NextNode):
+        op = node.operand
+
+        if isinstance(op, AndNode):
+            new_node = AndNode(NextNode(op.left), op.right)
+            return MutationResult(new_node, MisconceptionCode.BadStateIndex)
+
+        if isinstance(op, NextNode):
+            x = op.operand
+            while isinstance(x, NextNode):
+                x = x.operand
+
+            return MutationResult(NextNode(x), MisconceptionCode.BadStateIndex)
+
+    return MutationResult(node)
---
+++ python-src/ltlnode.py
@@ -0,0 +1,188 @@
+# Description: This file contains the classes for the nodes of the LTL syntax tree.
+
+from ltlListener import ltlListener
+from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker, CharStreams
+from antlr4 import ParseTreeWalker, CommonTokenStream, InputStream
+from ltlLexer import ltlLexer
+from ltlParser import ltlParser
+
+
+class LTLNode:
+    def __init__(self, type):
+        self.type = type
+
+    def __str__(self):
+        pass
+
+    @staticmethod
+    def equiv(a, b):
+        return str(a) == str(b)
+
+
+
+class ltlListenerImpl(ltlListener) :
+    def __init__(self):
+        self.stack = []
+
+    def exitDisjunction(self, ctx):
+        right = self.stack.pop()
+        left = self.stack.pop()
+        orNode = OrNode(left, right)
+        self.stack.append(orNode)
+
+    def exitConjunction(self, ctx):
+        right = self.stack.pop()
+        left = self.stack.pop()
+        andNode = AndNode(left, right)
+        self.stack.append(andNode)
+
+    def exitUntil(self, ctx):
+        right = self.stack.pop()
+        left = self.stack.pop()
+        untilNode = UntilNode(left, right)
+        self.stack.append(untilNode)
+
+    def exitImplication(self, ctx):
+        right = self.stack.pop()
+        left = self.stack.pop()
+        impliesNode = ImpliesNode(left, right)
+        self.stack.append(impliesNode)
+
+    def exitEquivalence(self, ctx):
+        right = self.stack.pop()
+        left = self.stack.pop()
+        equivNode = EquivalenceNode(left, right)
+        self.stack.append(equivNode)
+
+    def exitX(self, ctx):
+        operand = self.stack.pop()
+        nextNode = NextNode(operand)
+        self.stack.append(nextNode)
+
+    def exitF(self, ctx):
+        operand = self.stack.pop()
+        finallyNode = FinallyNode(operand)
+        self.stack.append(finallyNode)
+
+    def exitG(self, ctx):
+        operand = self.stack.pop()
+        globallyNode = GloballyNode(operand)
+        self.stack.append(globallyNode)
+
+    def exitNot(self, ctx):
+        operand = self.stack.pop()
+        notNode = NotNode(operand)
+        self.stack.append(notNode)
+
+    def exitParentheses(self, ctx):
+        formula = self.stack.pop()
+        self.stack.append(formula)
+
+    def exitAtomicFormula(self, ctx):
+        value = ctx.ID().getText()
+        literalNode = LiteralNode(value)
+        self.stack.append(literalNode)
+
+    def getRoot(self):
+        return self.stack[-1]
+
+
+class UnaryOperatorNode(LTLNode):
+    def __init__(self, operator, operand):
+        super().__init__('UnaryOperator')
+        self.operator = operator
+        self.operand = operand
+
+    def __str__(self):
+        return f'({self.operator} {str(self.operand)})'
+
+
+class BinaryOperatorNode(LTLNode):
+    def __init__(self, operator, left, right):
+        super().__init__('BinaryOperator')
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f'({str(self.left)} {self.operator} {str(self.right)})'
+
+
+class LiteralNode(LTLNode):
+    def __init__(self, value):
+        super().__init__('Literal')
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class UntilNode(BinaryOperatorNode):
+    def __init__(self, left, right):
+        super().__init__('U', left, right)
+
+
+class NextNode(UnaryOperatorNode):
+    def __init__(self, operand):
+        super().__init__('X', operand)
+
+
+class GloballyNode(UnaryOperatorNode):
+    def __init__(self, operand):
+        super().__init__('G', operand)
+
+
+class FinallyNode(UnaryOperatorNode):
+    def __init__(self, operand):
+        super().__init__('F', operand)
+
+
+class OrNode(BinaryOperatorNode):
+    def __init__(self, left, right):
+        super().__init__('||', left, right)
+
+
+class AndNode(BinaryOperatorNode):
+    def __init__(self, left, right):
+        super().__init__('&&', left, right)
+
+
+class NotNode(UnaryOperatorNode):
+    def __init__(self, operand):
+        super().__init__('!', operand)
+
+
+class ImpliesNode(BinaryOperatorNode):
+    def __init__(self, left, right):
+        super().__init__('=>', left, right)
+
+
+class EquivalenceNode(BinaryOperatorNode):
+    def __init__(self, left, right):
+        super().__init__('<=>', left, right)
+
+
+
+def parse_ltl_string(s):
+    # Create an input stream from the string
+    input_stream = InputStream(s)
+
+    # Create a lexer and a token stream
+    lexer = ltlLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+
+    # Create the parser and parse the input
+    parser = ltlParser(token_stream)
+    tree = parser.ltl()
+
+    # Create a listener
+    listener = ltlListenerImpl()
+
+    # Create a ParseTreeWalker and walk the parse tree with the listener
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+
+    # Get the root of the syntax tree
+    root = listener.get_root()
+
+    return root
