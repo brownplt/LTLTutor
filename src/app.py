3d548b07d22c2973a9006ed7f80ed2305bc1837a
Siddhartha Prasad <siddhartha.a.prasad@gmail.com>
2024-05-10 14:31:55 -0400

    adding stuff

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ import ast
 import exerciseprocessor
 import exercisebuilder
 import random
+import spotutils
 
 app = Flask(__name__)
 
@@ -40,16 +41,23 @@ def ltl():
 def loadfromjson():
     return render_template('loadfromjson.html')
 
-@app.route('/authorquestion', methods=['POST'])
+@app.route('/authorquestion/', methods=['POST'])
 def authorquestion():
 
+    kind = request.form.get('kind')
     answer = request.form.get('answer')
     question = request.form.get('question')
     exercise_so_far = request.form.get('exercisesofar')
 
-    # Parse the LTL string
     try:
-        ltl = parse_ltl_string(answer)
+        if kind == "tracesatisfaction":
+            ltl = parse_ltl_string(question)
+
+        elif kind == "englishtoltl":
+            ltl = parse_ltl_string(answer)
+        else:
+            return "Invalid question type"
+        
         d = getAllApplicableMisconceptions(ltl)
         distractors = []
         for misconception in d:
@@ -66,20 +74,48 @@ def authorquestion():
                 existingDistractor['code'] += f", {distractor['code']}"
             else:
                 mergedDistractors.append(distractor)
+
         distractors = mergedDistractors
 
+
+
+
+        new_distractors = []
+        added_traces = set([answer])
+        ## IF the kind is trace satisfaction, we need to generate traces for each distractor:
+        if kind == "tracesatisfaction":
+
+            answer_formula = str(ltl)
+            
+            for distractor in distractors:
+                f = distractor['formula']
+                potential_trace_choices = spotutils.generate_traces(f_accepted=f, f_rejected=answer_formula, max_traces=10)
+                trace_choices = [t for t in potential_trace_choices if t not in added_traces]
+                if len(trace_choices) > 0:
+                    c = random.choice(trace_choices)
+                    ms = distractor['code']
+                    new_distractors.append({
+                        'formula': c,
+                        'code': ms
+                    })
+                    added_traces.add(c)
+
+        distractors = new_distractors
         if len(distractors) == 0:
             distractors.append({
                 "formula": "-",
                 "code": "Could not determine applicable distractors."
             })
-        return render_template('authorquestion.html', distractors=distractors, error="", answer=answer, question=question, exerciseset = exercise_so_far)
+
+        return render_template('authorquestion.html', distractors=distractors, error="", answer=answer, question=question, exerciseset = exercise_so_far, kind = kind)
+    
+    
     except Exception as e:
         distractors = [{
             "formula": "-",
             "code": "Invalid LTL formula"
         }]
-        return render_template('authorquestion.html', error='Invalid LTL formula', distractors=distractors, answer=answer, question=question, exerciseset = exercise_so_far)
+        return render_template('authorquestion.html', error='Invalid LTL formula', distractors=distractors, answer=answer, question=question, exerciseset = exercise_so_far, kind = kind)
 
 
 
