6fa94d78dae767b56b4deba24f80eda49eea9cff
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2024-07-09 12:23:45 -0400

    Version update

--- src/static/js/common-functionality.js
+++ src/static/js/common-functionality.js
@@ -1,58 +1,4 @@
 
-//const USERIDKEY = "ltluserid";
-
-// function getCookie(name) {
-//     let cookieArray = document.cookie.split(';');
-//     for (let i = 0; i < cookieArray.length; i++) {
-//         let cookie = cookieArray[i];
-//         while (cookie.charAt(0) == ' ') {
-//             cookie = cookie.substring(1);
-//         }
-//         if (cookie.indexOf(name) == 0) {
-//             return cookie.substring(name.length + 1);
-//         }
-//     }
-//     return "";
-// }
-
-
-
-// async function ensureUserId() {
-//     // Check if the cookie exists
-//     var noUserId = document.cookie.indexOf(USERIDKEY) === -1 || getCookie(USERIDKEY) == null || getCookie(USERIDKEY) == "";
-//     var userId = Math.random().toString(36).substring(2, 15)
-//     if (noUserId) {
-//         $('#userIdModal').removeClass('d-none');
-//         try {
-//             const response = await fetch('/getuserid');
-//             userId = await response.text();
-//         } catch (error) {
-//             console.error('Error Generating User Id:', error);
-//             console.log("Defaulting to less memorable user ID: " + userId)
-
-//         }
-//         // Set the cookie with the user ID
-//         const date = new Date();
-//         date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
-//         const expires = ";expires=" + date.toUTCString();
-
-//         document.cookie = USERIDKEY + "=" + userId + expires + ";path=/";
-//     }
-
-// }
-
-// $(document).ready(function () {
-//     ensureUserId()
-//     .then(() => {
-//         // Get the value of the cookie ltluserid and store it in a variable
-//         var userId = getCookie(USERIDKEY);
-//         var uidfield = document.getElementById("userid_field");
-
-//         if (uidfield) {
-//             uidfield.innerText = "User: " + userId;
-//         }
-//     });
-// });
 
 
 
@@ -64,3 +10,18 @@ function scrollToFeedback() {
     }, 100);
 
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.help').forEach(function(element) {
+        element.addEventListener('click', function() {
+            // Extract the text content of the parent .help element
+            var helpText = this.innerHTML;
+            
+            // Set the content of the modal dynamically
+            document.querySelector('#genericHelpModal .modal-body').innerHTML = helpText;
+            
+            // Trigger the modal to open
+            $('#genericHelpModal').modal('show');
+        });
+    });
+});
