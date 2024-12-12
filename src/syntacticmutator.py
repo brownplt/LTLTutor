17cdc378c2a5ce60b376a89cc2273abbf03976d1
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2024-12-12 11:09:26 -0500

    A start

---
+++ src/syntacticmutator.py
@@ -0,0 +1,15 @@
+from ltlnode import *
+
+
+
+def applyRandomMutationNotEquivalentTo(node, notEquivalentToNodes):
+    
+    # Ensures that the mutated node is not equivalent to any of the nodes in the list
+
+
+    
+    return node
+
+
+def applyRandomMutation(node):
+    return node
